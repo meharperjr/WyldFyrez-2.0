ec3537f0e78e9a0c4264803d72772d124947b27e
unknown <meharperjr@gmail.com>
2019-11-24 11:14:18 -0800

    updating config file for public use

--- Dashboard/js/config.js
+++ Dashboard/js/config.js
@@ -1,3 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoibWVoYXJwZXJqciIsImEiOiJjazB0dDVucHcwYTM4M21vMGFrcGUzY2U2In0.zomxvDnDeASmphS_iSHwpg";
-const API_Key_TM= "eGKcmHEptGA4YbpqYAfiCHyVzPojhPSs";
+const API_KEY = "pk.eyJ1IjoibWVoYXJwZXJqciIsImEiOiJjazNkZHFzd2QxOGhpM29vMTBybXBrcWlzIn0.KcIQcRMo6nCCNP-H1LNhKA";
